f7ce43c684368906ba20532c681885bb01d040b5
Ezechiel Felten <108800520+ezeflt@users.noreply.github.com>
2023-08-19 10:02:15 +0200

    Update places.js

--- backend/routes/places.js
+++ backend/routes/places.js
@@ -2,55 +2,69 @@ var express = require('express');
 var router = express.Router();
 const Place = require('../models/places')
 
-    //ajout d'une ville en BDD
-    router.post('/places', (req,res)=>{
-        console.log('reqqqq',req.body);
-        // si les champs sont pas remplis stop la fonction
+    /**
+     * Description :
+     * add a place to the database
+     */
+    router.post('/add', (req,res)=>{
+
+        // check if POST datas is empty
         if ((!req.body.nickname)||(!req.body.name)||(!req.body.latitude)||(!req.body.longitude)) {
-            res.json({ result: false, error: 'il faut tout remplir' });
+            res.json({ result: false, error: 'field the input' });
             return;
           }
 
-          // si la ville n'est pas déjà enregistrer à un utilisateur
-          Place.findOne({nickname: req.body.nickname, name:req.body.name})
-          .then(data=>{
-            if(!data){
-                //crée une nouvelle ville à cette utilisateur
-                const newPlace = new Place({
-                    nickname: req.body.nickname,
-                    name:req.body.name,
-                    latitude:req.body.latitude,
-                    longitude:req.body.longitude,
-                })
-                newPlace.save().then(newPlace=>{
-                    // ajoute le en BDD
-                    res.json({true:'the place is add'})
-                })
-            }else{
-                res.json({err:'place existe déjà'})
-            }
-          })
+        // find a place by nickanme and city name
+        Place.findOne({nickname: req.body.nickname, name:req.body.name})
+        .then(data=>{
+        // if place is not found, create a place
+        if(!data){
+            const newPlace = new Place({
+                nickname: req.body.nickname,
+                name:req.body.name,
+                latitude:req.body.latitude,
+                longitude:req.body.longitude,
+            })
+            newPlace.save() // save the place to the database
+            res.json({true:'the place is added'}) // return the route response (valid reponse)
+        }else{
+            res.json({err:'the place already exists'}) // return the route response (fail response)
+        }
+        })
 
     })
 
-    //depuis le nom d'une ville en params je récupère ses données
+    /**
+     * Description :
+     * GET user places from the database
+     * 
+     * @param {any} req require the user nickame
+     * @param {any} res is the route response in json format
+     */
     router.get('/places/:nickname',(req,res)=>{
 
+        // find user places by user nickname
         Place.find({nickname: req.params.nickname})
         .then(data=>{
-            if(data){
-                res.json({result: true, places: data})
-            }
+            data ? res.json({result: true, places: data}) : res.json({result: false, }); // send the route response
         })
         
     })
-    // avec le nom de la ville en params je le supprime de la BDD
-    router.delete('/places',(req, res)=>{
-        Place.deleteOne({nickname: req.body.nickname, name: req.body.name}).then(()=>{
-            Place.find().then(data=>{
-                res.json({true:'is delete'})
-            })
-        })
+
+    /**
+     * Description :
+     * delete a place from the database 
+     * 
+     * @param {any} req require the user nickame and the city name
+     * @param {any} res is the route response in json format
+     */
+    router.delete('/delete',(req, res)=>{
+
+        // delete a place  by user nickname and the cityname
+        Place.deleteOne({nickname: req.body.nickname, name: req.body.name})
+
+        // send the route response
+        res.json({ response :'the place is delete'});
     })
 
 module.exports = router;
